fad8d336abf514f4098ea5b787b25857989c605c
墨綠BG <s5460703@gmail.com>
2024-07-12 01:25:48 +0800

    🔨 refactor：调整登录注册样式

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,15 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const cover = 'https://t.tutu.to/img/m1IOz';
+
   return (
-    <div className="flex h-full items-center justify-center">{children}</div>
+    <div
+      className="flex h-full items-center justify-center"
+      style={{
+        background: `url(${cover}) center/cover no-repeat`,
+      }}
+    >
+      <div className="md:-ml-[40%]">{children}</div>
+    </div>
   );
 };
 
