89c2812c986ceff0252f0a21f67d429803c9d2af
墨綠BG <s5460703@gmail.com>
2024-07-11 10:39:32 +0800

    🌈 style：代码格式化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import '@/common/styles/globals.css';
+import '@/common/styles/globals.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
