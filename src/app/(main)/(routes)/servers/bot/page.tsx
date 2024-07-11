21ff99657435aeb9ac4c52f340c80f0e2c12d2bb
墨綠BG <s5460703@gmail.com>
2024-07-12 03:12:32 +0800

    🔨 refactor：更新机器人链接

--- src/app/(main)/(routes)/servers/bot/page.tsx
+++ src/app/(main)/(routes)/servers/bot/page.tsx
@@ -4,7 +4,7 @@ const Page: NextPage = () => {
   return (
     <>
       <iframe
-        src="https://www.chatbase.co/chatbot-iframe/637zQZNRXyVYZTiVKTwQe"
+        src="https://www.chatbase.co/chatbot-iframe/RgGQ5iD4b0AI4tswuRF31"
         title="Chatbot"
         width="100%"
         className="h-full min-h-[700px]"
