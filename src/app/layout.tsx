9d52990d08b2d03199f3a4334720585cc3f3d925
墨綠BG <s5460703@gmail.com>
2024-07-11 17:42:55 +0800

    📃 docs：撰写项目详细信息

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ import '@/common/styles/globals.scss';
 const font = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Harmony - 一个在线即时通讯软件',
+  title: 'Harmony - 在线即时通讯平台',
   description: '爱来自华中师范大学 2022 级计算机学院早安少女组',
 };
 
