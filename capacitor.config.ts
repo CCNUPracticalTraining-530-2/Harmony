aa3da7a60c894342ba2a5f550525a5a2923fa8d4
墨綠BG <s5460703@gmail.com>
2024-07-12 16:11:31 +0800

    ✨ feat：新增客户端 socket

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.harmony.app',
   appName: 'Harmony',
-  webDir: 'out'
+  webDir: 'out',
 };
 
 export default config;
